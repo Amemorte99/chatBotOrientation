53a95931d5f905c1ea01b6047d30571b7e64d1ed
Amemorete <koisonnmanes@gmail.com>
2023-10-27 19:23:59 +0000

    chat best version manes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from __future__ import annotations
 
 import nltk
@@ -5,46 +6,44 @@ import json
 from difflib import get_close_matches
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
+from nltk.stem import SnowballStemmer
 from nltk.tag import pos_tag
 from nltk.chunk import ne_chunk
 
 nltk.download('punkt')  # Télécharge les modèles tokenizer
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
+nltk.download('stopwords')  # Télécharge les mots vides (stopwords) en français
+nltk.download('averaged_perceptron_tagger')  # Télécharge le modèle POS tagger
+nltk.download('maxent_ne_chunker')  # Télécharge le modèle de Named Entity Recognition (NER)
+nltk.download('words')  # Télécharge les mots communs en anglais (nécessaire pour le POS tagger)
+
+stemmer = SnowballStemmer('french')
 
 
-def load_knowledge_base(file_path: str) -> dict:
-    with open(file_path, 'r') as file:
+def load_intents(file_path: str) -> dict:
+    with open(file_path, 'r', encoding='utf-8') as file:
         data: dict = json.load(file)
     return data
 
 
-def save_knowledge_base(file_path: str, data: dict):
-    with open(file_path, 'w') as file:
-        json.dump(data, file, indent=2)
+def save_intents(file_path: str, data: dict):
+    with open(file_path, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
 
 
-def find_best_match(user_question: str, questions: list) -> str | None:
-    lemmatizer = WordNetLemmatizer()
+def find_best_match(user_question: str, patterns: list) -> str | None:
     stop_words = set(stopwords.words('french'))
 
-    user_tokens = [lemmatizer.lemmatize(token.lower()) for token in word_tokenize(user_question) if
-                   token.lower() not in stop_words]
+    user_tokens = [stemmer.stem(token.lower()) for token in word_tokenize(user_question) if token.lower() not in stop_words]
     best_match = None
     best_similarity = 0
 
-    for question in questions:
-        question_tokens = [lemmatizer.lemmatize(token.lower()) for token in word_tokenize(question) if
-                           token.lower() not in stop_words]
-        similarity = len(set(user_tokens) & set(question_tokens)) / len(set(user_tokens) | set(question_tokens))
+    for pattern in patterns:
+        pattern_tokens = [stemmer.stem(token.lower()) for token in word_tokenize(pattern) if token.lower() not in stop_words]
+        similarity = len(set(user_tokens) & set(pattern_tokens)) / len(set(user_tokens) | set(pattern_tokens))
 
         if similarity > best_similarity:
             best_similarity = similarity
-            best_match = question
+            best_match = pattern
 
     if best_similarity >= 0.4:
         return best_match
@@ -52,14 +51,14 @@ def find_best_match(user_question: str, questions: list) -> str | None:
         return None
 
 
-def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
-    for q in knowledge_base["questions"]:
-        if q["question"] == question:
-            return q["answers"][q["correctAnswer"]]
+def get_response_for_intent(intent_tag: str, intents: dict) -> str | None:
+    for intent in intents["intents"]:
+        if intent["tag"] == intent_tag:
+            return intent["responses"]
 
 
-def get_closest_match(user_input: str, questions: list) -> str | None:
-    close_match = get_close_matches(user_input, questions, n=1, cutoff=0.8)
+def get_closest_match(user_input: str, patterns: list) -> str | None:
+    close_match = get_close_matches(user_input, patterns, n=1, cutoff=0.8, dictionary=patterns)
     if close_match:
         return close_match[0]
     else:
@@ -79,7 +78,7 @@ def extract_named_entities(text: str) -> list:
 
 def chat_bot():
     print(nltk.__version__)
-    knowledge_base: dict = load_knowledge_base('orientation_esgis_base.json')
+    intents: dict = load_intents('orientation_esgis_base.json')
 
     while True:
         user_input: str = input('Vous: ')
@@ -87,14 +86,17 @@ def chat_bot():
         if user_input.lower() == 'quit':
             break
 
-        best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
+        best_match: str | None = find_best_match(user_input, [p for intent in intents["intents"] for p in intent["patterns"]])
 
         if not best_match:
-            best_match = get_closest_match(user_input, [q["question"] for q in knowledge_base["questions"]])
+            best_match = get_closest_match(user_input, [p for intent in intents["intents"] for p in intent["patterns"]])
 
         if best_match:
-            answer: str = get_answer_for_question(best_match, knowledge_base)
-            print(f'Bot: {answer}')
+            intent_tag = [intent["tag"] for intent in intents["intents"] if best_match in intent["patterns"]][0]
+            responses = get_response_for_intent(intent_tag, intents)
+            if responses:
+                response = responses[0]  # Choix de la première réponse pour simplifier
+                print(f'Bot: {response}')
         else:
             named_entities = extract_named_entities(user_input)
             if named_entities:
@@ -104,9 +106,13 @@ def chat_bot():
             new_answer: str = input('Tapez la réponse ou "Skip" pour passer : ')
 
             if new_answer.lower() != 'skip':
-                knowledge_base["questions"].append({"question": user_input, "answers": new_answer})
-                save_knowledge_base('orientation_esgis_base.json', knowledge_base)
+                # Ajouter la nouvelle intention au JSON
+                intent_tag = "custom_intent"
+                intents["intents"].append({"tag": intent_tag, "patterns": [user_input], "responses": [new_answer]})
+                save_intents('intents.json', intents)
                 print('Bot: Merci ! J\'ai appris une nouvelle réponse !')
+
+
 
 
 if __name__ == '__main__':
