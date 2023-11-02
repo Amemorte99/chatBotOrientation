ec4b9e44e1465ea8ff3ba9a4c6772462e19a7236
Amemorete <koisonnmanes@gmail.com>
2023-11-02 21:51:20 +0000

    r

--- main3.py
+++ main3.py
@@ -9,7 +9,7 @@ from keras.preprocessing.text import Tokenizer
 from keras import optimizers
 from keras.optimizers import Adam
 from keras.src.layers import Dropout
-from sklearn.preprocessing import LabelEncoder  # Import correct LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder  # Import correct LabelEncoder
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
@@ -17,7 +17,9 @@ from nltk.tag import pos_tag
 from nltk.chunk import ne_chunk
 from difflib import get_close_matches
 from keras.utils import to_categorical
-
+import pandas as pd
+import nltk
+import numpy as np
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -27,6 +29,15 @@ nltk.download('words')
 
 stemmer = SnowballStemmer('french')
 
+
+
+def load_csv(file_path: str) -> pd.DataFrame:
+    return pd.read_csv(file_path, encoding='utf-8')
+
+def process_input(input_text):
+    stop_words = set(stopwords.words('french'))
+    tokens = [stemmer.stem(token.lower()) for token in word_tokenize(input_text) if token.lower() not in stop_words]
+    return tokens
 
 def load_intents(file_path: str) -> dict:
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -92,15 +103,14 @@ def get_response_for_intent(intent_tag: str, intents: dict) -> str | None:
 
 
 def chat_bot():
-    intents = load_intents('orientation_esgis_base.json')
+    # Chargement des données depuis un fichier CSV
+    df = pd.read_csv('orientation_esgis_base.csv')
 
-    # Préparation des motifs et des tags pour l'entraînement
-    patterns = []
-    tags = []
-    for intent in intents["intents"]:
-        for pattern in intent["patterns"]:
-            patterns.append(pattern)
-            tags.append(intent["tag"])
+    # Assurez-vous que votre CSV a les colonnes correctes. Ici, nous supposons qu'il y a plusieurs colonnes de motifs.
+    # Exemple de noms de colonnes: intents/patterns/0, intents/patterns/1, etc.
+    pattern_columns = [col for col in df.columns if 'intents/patterns' in col]
+    patterns = df[pattern_columns].apply(lambda row: ' '.join(row.values.astype(str)), axis=1).tolist()
+    tags = df['intents/tag'].tolist()  # Changez 'intents/tag' en nom correct de colonne pour les tags si nécessaire
 
     # Création du tokenizer et conversion des motifs en séquences numériques
     tokenizer = Tokenizer()
@@ -110,52 +120,46 @@ def chat_bot():
     # Encodage des tags
     label_encoder = LabelEncoder()
     tags_encoded = label_encoder.fit_transform(tags)
-
-    # Vérification de la correspondance entre les séquences et les tags encodés
-    print(f'Nombre de motifs: {len(patterns)}')
-    print(f'Nombre de tags: {len(tags)}')
-    print(f'Nombre de séquences: {len(sequences)}')
-    print(f'Nombre de tags encodés: {len(tags_encoded)}')
-
-    # S'assurer que le nombre de séquences et de tags encodés est le même
-    assert len(sequences) == len(tags_encoded), "Le nombre de séquences et de tags encodés ne correspond pas."
+    tags_encoded = to_categorical(tags_encoded)
 
     # Padding des séquences pour avoir une longueur uniforme
-    max_sequence_length = max([len(seq) for seq in sequences])  # Dynamiquement déterminé à partir des données
+    max_sequence_length = max(len(seq) for seq in sequences)
     X = pad_sequences(sequences, padding='post', maxlen=max_sequence_length)
 
     # Définition de l'architecture du modèle
-    # model = Sequential()
-    # model.add(Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=128, input_length=max_sequence_length))
-    # model.add(LSTM(100))
-    # model.add(Dense(len(set(tags_encoded)), activation='softmax'))
-
-    label_encoder = LabelEncoder()
-    tags_encoded = label_encoder.fit_transform(tags)
-    tags_encoded = to_categorical(tags_encoded)  # Convert to one-hot encoding
-
-    # ... (No changes until model definition)
-
     model = Sequential()
     model.add(Dense(128, activation='relu', input_shape=(X.shape[1],)))
     model.add(Dropout(0.5))
     model.add(Dense(256, activation='relu'))
-    model.add(Dense(tags_encoded.shape[1], activation='softmax'))  # Using the second dimension of tags_encoded
+    model.add(Dense(tags_encoded.shape[1], activation='softmax'))
 
     # Compilation du modèle
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-    # ... (No changes until the fit function)
-
-    # Conversion des tags encodés en un tableau numpy pour la compatibilité avec Keras est déjà faite
-    # Pas besoin de convertir encore une fois car `tags_encoded` est déjà un tableau numpy grâce à `to_categorical`
-
-
-
     # Entraînement du modèle
-    model.fit(X, tags_encoded, epochs=100)
+    model.fit(X, tags_encoded, epochs=1000)
 
+    # Fonction pour traiter l'entrée de l'utilisateur
+    def process_input(user_input):
+        # Vous devriez définir cette fonction pour traiter l'entrée de l'utilisateur
+        # Par exemple, cela pourrait être la tokenisation et la suppression des stopwords
+        return user_input.lower()
 
+    # Fonction pour obtenir la réponse pour une intention donnée
+    def get_response_for_intent(intent_tag):
+        # Supposons que vous avez un dictionnaire qui mappe les tags d'intention aux réponses
+        responses = {
+            "salutation": "Bonjour! Comment puis-je vous aider?",
+            "formation_info": "Nous offrons une variété de formations. Pour quel domaine êtes-vous intéressé?",
+            "profil_entree": "Le profil d'entrée requiert généralement un bac scientifique ou économique.",
+
+            # Ajoutez autant de réponses que nécessaire pour les autres tags
+        }
+
+        # Retourner la réponse correspondant au tag d'intention, ou une réponse par défaut
+        return responses.get(intent_tag, "Je suis désolé, je ne comprends pas votre demande.")
+
+    # Dialogue avec l'utilisateur
     try:
         while True:
             user_input = input('Vous: ')
@@ -163,24 +167,28 @@ def chat_bot():
                 break
 
             user_tokens = process_input(user_input)
-            user_input_sequence = pad_sequences(tokenizer.texts_to_sequences([user_tokens]), padding='post', maxlen=max_sequence_length)
-
-            intent_prediction = model.predict(user_input_sequence)
+            user_sequence = pad_sequences(tokenizer.texts_to_sequences([user_tokens]), padding='post', maxlen=max_sequence_length)
+            intent_prediction = model.predict(user_sequence)
             intent_index = np.argmax(intent_prediction)
             intent_tag = label_encoder.inverse_transform([intent_index])[0]
 
-            if intent_prediction[0][intent_index] < 0.8:
-                closest_match = get_closest_match(user_input, patterns)
-                if closest_match:
-                    intent_tag = find_best_match(closest_match, patterns)
+            response = get_response_for_intent(intent_tag)
+            print("Assistant:", response)
 
-            response = get_response_for_intent(intent_tag, intents)
-            if response:
-                print("Assistant:", response)
-            else:
-                print("Assistant: Je suis désolé, je ne peux pas répondre à cette question pour le moment.")
     except Exception as e:
         print(f"Une erreur est survenue: {e}")
+
+def get_response_for_intent(intent):
+    # Fonction fictive pour récupérer la réponse en fonction de l'intention
+    # Vous devez implémenter cette fonction pour correspondre à vos propres données et logique de réponse
+    if intent == 'greeting':
+        return 'Bonjour ! Comment puis-je vous aider ?'
+    elif intent == 'farewell':
+        return 'Au revoir ! À bientôt.'
+    elif intent == 'question':
+        return 'Je suis désolé, je ne peux pas répondre à cette question pour le moment.'
+    else:
+        return 'Je ne comprends pas. Pouvez-vous reformuler votre question ?'
 
 if __name__ == '__main__':
     chat_bot()
